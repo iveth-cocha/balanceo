24ce4fd6e2a9006a5e0c42ff84791df7b438e588
ivteh-cocha <maria.iveth16@outlook.es>
2024-07-01 13:36:52 -0500

    first commit

---
+++ Servidor3/servidor3.py
@@ -0,0 +1,9 @@
+from flask import Flask
+servidor= Flask(__name__)
+@servidor.route('/')
+
+def hola():
+    return "hola desde el servidor 3 HI"
+
+if __name__=='__main__':
+    servidor.run(host='0.0.0.0')
